62152adf3d72ce3136b55819c95e37d1ab1c19ff
Aureatus <93552500+Aureatus@users.noreply.github.com>
2024-06-23 03:07:18 +0100

    Add auto reset functionality

--- lib/bubbles_web/live/bubble_live.ex
+++ lib/bubbles_web/live/bubble_live.ex
@@ -5,7 +5,12 @@ defmodule BubblesWeb.BubbleLive do
     ~H"""
     <div class="flex flex-col w-max h-max">
       <.header>Score: <%= @score %></.header>
-      <.button phx-click="reset_bubbles" class="w-full">Reset bubbles</.button>
+      <.button phx-click="reset_bubbles" class="w-full">
+        Reset bubbles
+      </.button>
+      <.button disabled={@score < 3 || @auto_reset} phx-click="enable-auto_reset" class="w-full">
+        Purchase auto reset
+      </.button>
       <div class="grid grid-cols-10 grid-rows-10 border-2 w-fit h-fit">
         <%= for {c, cindex} <- Enum.with_index(@bubbles) do %>
           <%= for {r, rindex} <- Enum.with_index(c) do %>
@@ -29,8 +34,11 @@ defmodule BubblesWeb.BubbleLive do
   end
 
   def mount(_params, _session, socket) do
-    bubbles = List.duplicate(false, 10) |> Enum.map(fn _x -> List.duplicate(false, 10) end)
-    {:ok, assign(socket, %{bubbles: bubbles, score: 0})}
+    :timer.send_interval(3000, self(), :tick)
+
+    assigns = %{bubbles: create_empty_bubbles(), score: 0, auto_reset: false}
+
+    {:ok, assign(socket, assigns)}
   end
 
   def handle_event("pop_bubble", value, socket) do
@@ -62,11 +70,49 @@ defmodule BubblesWeb.BubbleLive do
         socket,
         :bubbles,
         fn _ ->
-          List.duplicate(false, 10) |> Enum.map(fn _x -> List.duplicate(false, 10) end)
+          create_empty_bubbles()
         end
       )
 
     {:noreply, socket}
+  end
+
+  def handle_event("enable-auto_reset", _, socket) do
+    case socket.assigns[:score] do
+      n when n < 3 ->
+        {:noreply, socket}
+
+      n when n >= 3 ->
+        socket =
+          update(
+            socket,
+            :auto_reset,
+            fn _ -> true end
+          )
+          |> update(:score, fn x -> x - 3 end)
+
+        {:noreply, socket}
+    end
+  end
+
+  def handle_info(:tick, socket) do
+    auto_reset? =
+      socket.assigns[:auto_reset]
+
+    if auto_reset? do
+      socket =
+        update(
+          socket,
+          :bubbles,
+          fn _ ->
+            create_empty_bubbles()
+          end
+        )
+
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
   end
 
   defp merge_bubble_changes(bubbles, value) do
@@ -75,5 +121,9 @@ defmodule BubblesWeb.BubbleLive do
       value["column"] |> String.to_integer(),
       &List.update_at(&1, value["row"] |> String.to_integer(), fn _ -> true end)
     )
+  end
+
+  defp create_empty_bubbles do
+    List.duplicate(false, 10) |> Enum.map(fn _x -> List.duplicate(false, 10) end)
   end
 end
